91b8df78c5bcf49ecdf23a8db258ecb28d503051
nguyenhuynhut <nguyenhuynhutsimple@gmail.com>
2011-02-25 14:46:23 +0700

    'first'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liquorlicensehq::Application.config.secret_token = 'd787eeffecaf89729f1d7386f072895dbb9a7b1edb38384410961d5b6007beaab1b096938ad0e9f0a64591d2788592275c278b24f8cea5feb2121cb9fb57c84e'
