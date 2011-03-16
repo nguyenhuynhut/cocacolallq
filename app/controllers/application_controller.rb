9fd4af49664a52657a18f21d2e89187368dbf502
nguyenhuynhut <nguyenhuynhutsimple@gmail.com>
2011-03-16 16:13:59 +0700

    'dd'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base 
   include SortableTable::App::Controllers::ApplicationController 
   protect_from_forgery
-  rescue_from User::NotAuthorized, :with => :user_not_authorized
-
-  private
-
-  def user_not_authorized
-    flash[:error] = "You don't have access to this section." 
-    redirect_to :back
-  end
+  
 end
